d8fb1dfcbe97c159312f1de6d796b7431a17fabf
vinayakkhadye <vinayak249@gmail.com>
2019-01-23 17:47:56 +0530

    socket code inital state

--- server/server.js
+++ server/server.js
@@ -1,13 +1,23 @@
 const path      = require('path');
+const socketIO  = require('socket.io');
+const http      = require('http');
 const express   = require('express');
 const publicPath= path.join(__dirname,'../public');
 
 var app     = express();
+var server  = http.createServer(app);
+var io      = socketIO(server);
 var port    = process.env.PORT || 3000;
 
 app.use( express.static(publicPath))
 
-app.listen(port,()=>{
+io.on('connection',(socket)=>{
+    console.log('New user connected');
+    socket.on('disconnect',()=>{
+        console.log('user disconnected');
+    })
+});
+server.listen(port,()=>{
     console.log(`server listening on port ${port}`)
 })
 
