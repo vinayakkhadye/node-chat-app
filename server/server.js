8acd7b85c2244d020d2be0dbca8f66a2473cd630
vinayakkhadye <vinayak249@gmail.com>
2019-01-23 16:35:23 +0530

    port added

--- server/server.js
+++ server/server.js
@@ -3,11 +3,11 @@ const express   = require('express');
 const publicPath= path.join(__dirname,'../public');
 
 var app     = express();
-var port    = 3000;
+var port    = process.env.PORT || 3000;
 
 app.use( express.static(publicPath))
 
 app.listen(port,()=>{
-    console.log('server listening on port http://localhost:3000')
+    console.log(`server listening on port ${port}`)
 })
 
